ba63af5c4cf589478611d8ab578b2e8bec4428cd
Gavin Quirk <gavinquirk@gmail.com>
2019-11-24 18:27:35 -0800

    get user position on front end, send data to server in intervals, db connection

--- public/geolocation.js
+++ public/geolocation.js
@@ -1,1 +1,24 @@
-// const socket = io.connect('http://localhost:5000');
+console.log('Geolocation Loaded...');
+
+// Establish Websocket Connection
+const socket = io.connect('http://localhost:5000');
+
+// Send Location Data Every 3 Seconds
+setInterval(function() {
+  if (navigator.geolocation) {
+    // Get Location Data
+    navigator.geolocation.getCurrentPosition(position => {
+      const lat = position.coords.latitude;
+      const lng = position.coords.longitude;
+
+      console.log(lat, lng);
+      // Emit Location Data
+      socket.emit('location', {
+        lat,
+        lng
+      });
+    });
+  } else {
+    alert('Geolocation is not supported by this browser.');
+  }
+}, 3000);
--- server.js
+++ server.js
@@ -1,11 +1,33 @@
 const express = require('express');
 const socket = require('socket.io');
 const colors = require('colors');
+const dotenv = require('dotenv');
+const mongoose = require('mongoose');
 
+// Load Environment Variables
+dotenv.config({ path: './config/config.env' });
+
+// Connect to Database
+const connectDB = async () => {
+  const conn = await mongoose.connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true
+  });
+
+  console.log(`MongoDB Connected: ${conn.connection.host}`.cyan.underline.bold);
+};
+
+connectDB();
+
+// Initialize Express Application
 const app = express();
 
+// Establish Port
 const PORT = process.env.PORT || 5000;
 
+// Listen for server connections
 const server = app.listen(PORT, () => {
   console.log(`Application listening on port ${PORT}`);
 });
@@ -16,16 +38,12 @@ app.use(express.static('public'));
 // Socket Setup
 const io = socket(server);
 
+// Listen for socket connections
 io.on('connection', socket => {
   console.log(`Socket connection established (${socket.id})`.blue);
 
-  // Chat
-  socket.on('chat', data => {
-    io.sockets.emit('chat', data);
-  });
-
-  // Typing
-  socket.on('typing', data => {
-    socket.broadcast.emit('typing', data);
+  // Location
+  socket.on('location', position => {
+    console.log(position);
   });
 });
